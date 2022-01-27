275a7dfb1748f3444cbb77dbee2600394bb6dd90
unknown <doni.yukri@gmail.com>
2022-01-27 12:09:46 +0700

    fix error on deployment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
-"use strict";
-
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
@@ -41,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is ready");
+    res.send("API is running....");
   });
 }
 
@@ -50,8 +49,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(
-    `Server is on ${process.env.NODE_ENV} mode and running on port ${PORT}`
-  )
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
